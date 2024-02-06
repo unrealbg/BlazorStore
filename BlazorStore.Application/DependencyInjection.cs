83166535a1d99cb7e6aeda33ed72cc3fe8c0515c
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-02-07 00:33:55 +0200

    Add project files.

---
+++ BlazorStore.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorStore.Application
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
